91d2740f423287e93cfb4b550dd56d3690f91d69
Anniefig <amfig74@gmail.com>
2021-11-17 00:19:44 -0500

    second phase test file

--- testrecon2.sh
+++ testrecon2.sh
@@ -1,40 +1,139 @@
 #!/bin/bash
 echo "How would you like to use tshark?"
-        options1=("Read packet data" "Most frequent source IP" "Most frequent destination IP" "Other")
+        options1=("Read packet data" "Most frequent source IP" "Most frequent destination IP" "Quit")
                 select answer1 in "${options1[@]}"; do
                         case $answer1 in
                                 "Read packet data")
                                         read -r -p "PCAP file: "
-                                        locate $REPLY
-                                        tshark -r $REPLY
-                                        ;;
+					pcap=$REPLY
+                                        echo "Would you like to save to a file?"
+                                        options=("yes" "no")
+                                        select answer in "${options[@]}"; do
+                                                case $answer in 
+                                                        "yes")
+                                                                 tshark -r $pcap  > packet_data
+                                                                echo "Saved to file! -> packet_data"
+								echo "Would you like to run another report?"
+                                                                options=("yes" "no")
+                                                                        select answer in "${options[@]}"; do
+                                                                                case $answer in 
+                                                                                        "yes")
+                                                                                                $0 && exit
+                                                                                                ;;
+ 
+                                                                                        "no")
+                                                                                         	exit 0 ;;
+										esac
+									done ;; 
+                                                        "no")
+                                                                tshark -r $pcap
+								echo "Would you like to run another report?"
+								options=("yes" "no")
+                                        				select answer in "${options[@]}"; do
+                                                				case $answer in 
+                                                        				"yes")
+                                                           					$0 && exit
+                                                           					;;
+                 				                                esac
+                                        				 done ;;
+                        				                                "no")
+												exit 0 ;;
+						esac
+					done
+					;;
 				"Most frequent source IP")
 					read -r -p "PCAP file: "
-					locate $REPLY
-					tshark -r $REPLY -T fields -e ip.src | sort | uniq -c | sort -n
+					pcap=$REPLY
+                                        echo "Would you like to save to a file?"
+                                        options=("yes" "no")
+                                        select answer in "${options[@]}"; do
+                                                case $answer in 
+                                                        "yes")
+                                                                tshark -r $pcap -T fields -e ip.src | sort | uniq -c | sort -n > Most_Src_IP
+                                                                echo "Saved to file! -> Most_Src_IP"
+								echo "Would you like to run another report?"
+                                                                options=("yes" "no")
+                                                                        select answer in "${options[@]}"; do
+                                                                                case $answer in 
+                                                                                        "yes")
+                                                                                                $0 && exit
+                                                                                                ;;
+ 
+                                                                                        "no")
+                                                                                                exit 0 ;;
+                                                                                esac
+                                                                        done ;;
+                                                        "no")
+                                                                tshark -r $pcap -T fields -e ip.src | sort | uniq -c | sort -n
+                                                                echo "Would you like to run another report?"
+                                                                options=("yes" "no")
+                                                                        select answer in "${options[@]}"; do
+                                                                                case $answer in 
+                                                                                        "yes")
+                                                                                                $0 && exit
+                                                                                                ;;
+                                                                                esac
+                                                                         done ;;
+                                                                                        "no")
+                                                                                                exit 0 ;;
+                                                esac
+                                        done
 					;;
 				"Most frequent destination IP")
 					read -r -p "PCAP file: "
-					locate $REPLY
-					tshark -r $REPLY -T fields -e ip.dst | sort | uniq -c | sort -n
-					;;
-                                 "Other")
-					echo  "do you want to exit"
+					pcap=$REPLY
+                                        echo "Would you like to save to a file?"
+                                        options=("yes" "no")
+                                        select answer in "${options[@]}"; do
+                                                case $answer in 
+                                                        "yes")
+                                                                tshark -r $pcap -T fields -e ip.dst | sort | uniq -c | sort -n > Most_Dst_IP
+                                                                echo "Saved to file! -> Most_Dst_IP"
+                                                                echo "Would you like to run another report?"
+                                                                options=("yes" "no")
+                                                                        select answer in "${options[@]}"; do
+                                                                                case $answer in 
+                                                                                        "yes")
+                                                                                                $0 && exit
+                                                                                                ;;
+ 
+                                                                                        "no")
+                                                                                                exit 0 ;;
+                                                                                esac
+                                                                        done ;;
+                                                        "no")
+								tshark -r $pcap -T fields -e ip.src | sort | uniq -c | sort -n
+                                                                echo "Would you like to run another report?"
+                                                                options=("yes" "no")
+                                                                        select answer in "${options[@]}"; do
+                                                                                case $answer in 
+                                                                                        "yes")  
+                                                                                                $0 && exit
+                                                                                                ;;
+                                                                                esac
+                                                                         done ;;
+                                                                                        "no")
+                                                                                                exit 0 ;;
+                                                esac
+                                        done
+                                        ;;
+                                 "Quit")
+					echo  "Would you like to exit?"
 					options=("no" "yes")
 					select answer in "${options[@]}"; do
 						case $answer in 
 							"no")
 								echo "What would you like to do next?"
-								options=("Run another report")
+								options=("Run another report?")
 								select answer2 in "${options[@]}"; do
 									case $answer2 in
-										"Run another report")
-											$0
+										"Run another report?")
+											$0 && exit
 											;;
-							"yes")
-								exit 0;;
 									esac
-								done 
+								done ;;
+							"yes")
+								exit 0 ;;
                                                 esac
                                         done
 			esac
